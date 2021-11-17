a5f1d7a9ebd9a4791f4f8ff20a4e88e8cc8569af
Robin La Rondelle <robinlarondelle@hotmail.com>
2021-11-17 17:28:16 +0100

    Added DataTransferObjects

---
+++ PlatformService/DTO/PlatformReadDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace PlatformService.DTO;
+
+public class PlatformReadDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Publisher { get; set; }
+}
---
+++ PlatformService/DTO/PlatformWriteDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlatformService.DTO;
+
+public class PlatformWriteDTO
+{
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    public string Publisher { get; set; }
+
+    [Required]
+    public string Cost { get; set; }
+}

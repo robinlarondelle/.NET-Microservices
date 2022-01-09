05550815ee783fef2cff3a28ce6a783ec72c613d
Robin La Rondelle <robinlarondelle@hotmail.com>
2022-01-09 22:12:58 +0100

    Command Service PlatformController setup

---
+++ CommandService/Controllers/PlatformController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommandService.Controllers;
+
+[Route("api/command-service/[controller]")]
+[ApiController]
+public class PlatformController : ControllerBase
+{
+    public PlatformController()
+    {
+        
+    }
+
+    [HttpPost]
+    public ActionResult TestInboundConnection()
+    {
+        Console.WriteLine("request received");
+        return Ok("request received");
+    }
+}
